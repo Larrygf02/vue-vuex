96674e4a34cbaa2d2095d15b7aad55932ef9019b
Raul <raulgf0293@gmail.com>
2020-03-05 12:25:42 -0500

    Vue config vuefity

--- src/router.js
+++ src/router.js
@@ -2,11 +2,13 @@ import Vue from "vue";
 import Router from "vue-router";
 import Person from "./components/Person";
 import Tarea from "./components/Tareas";
+import About from "./views/About";
 Vue.use(Router);
 
 const routes = [
   { path: "/person", component: Person },
-  { path: "/tarea", component: Tarea }
+  { path: "/tarea", component: Tarea },
+  { path: "/about", component: About }
 ];
 export default new Router({
   mode: "history",
